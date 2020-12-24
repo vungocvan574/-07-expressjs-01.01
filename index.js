61128501e8fa5503593f4c346bf806a60bf9d2f6
vungoc.van.574 <vungoc.van.574@gmail.com>
2020-12-24 14:13:37 +0000

    view template

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ var app = express();
 
 var port = 3000;
 
+app.set('view engine', 'pug')
+app.set('views', './views')
+
 app.get('/', function(req, res) {
-    res.send('Hello world')
+    res.render('index', {
+        name: 'Ilya'
+    });
 });
 
+app.get('/users', function(req, res) {
+    res.render('users/index', {
+        users: [
+            { id: 1, name: 'Van' },
+            { id: 2, name: 'Hoi' }
+        ]
+    })
+})
+
 app.listen(port, function() {
-    console.log('Server is listening to //localhost:' + port);
+    console.log('Server is listening to localhost:' + port);
 })
