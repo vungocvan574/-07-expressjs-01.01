3127dee484048cbb2b1253a85f3e4243b009b2b2
vungoc.van.574 <vungoc.van.574@gmail.com>
2020-12-24 19:04:57 +0000

    user detail with short id

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var express = require('express');
-
 var app = express();
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+const { generate } = require('shortid');
+const adapter = new FileSync('db.json');
+var shortId = require('shortid');
+
+var db = low(adapter);
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [] })
+    .write();
 
 var port = 3000;
 
-app.set('view engine', 'pug')
-app.set('views', './views')
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
@@ -16,20 +25,29 @@ app.get('/', function(req, res) {
     });
 });
 
-var users = [
-    { id: 1, name: 'Van' },
-    { id: 2, name: 'Hoi' }
-]
-
 app.get('/users', function(req, res) {
     res.render('users/index', {
-        users: users
+        users: db.get('users').value()
+    });
+});
+
+app.get('/users/create', function(req, res) {
+    res.render('users/create')
+});
+
+
+app.get('/users/:id', function(req, res) {
+    var id = req.params.id;
+    var user = db.get('users').find({ id: id }).value();
+
+    res.render('users/user-detail', {
+        user: user
     });
 });
 
 app.get('/users/search', function(req, res) {
     var q = req.query.q;
-    var matchedUsers = users.filter(function(user) {
+    var matchedUsers = db.get('users').filter(function(user) {
         return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
     });
 
@@ -38,12 +56,9 @@ app.get('/users/search', function(req, res) {
     });
 });
 
-app.get('/users/create', function(req, res) {
-    res.render('users/create')
-});
-
 app.post('/users/create', function(req, res) {
-    users.push(req.body);
+    req.body.id = shortId.generate();
+    db.get('users').push(req.body).write();
     res.redirect('/users');
 });
 
